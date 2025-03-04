76a115ad6350ae54ae2f2a2b15075606b50bd42d
Similachi Andrei <sivfhd@yahoo.com>
2025-03-05 00:29:14 +0200

    added update and delete operations on the database

--- homework1/db.py
+++ homework1/db.py
@@ -29,16 +29,32 @@ class DbInteraction:
         );""")
         self.cur.execute("""CREATE TABLE books(
         id SERIAL PRIMARY KEY,
-        lib_id INT REFERENCES libraries(id),
+        lib_id INT REFERENCES libraries(id) ON DELETE CASCADE,
         name TEXT NOT NULL,
         genre TEXT NOT NULL
         );""")
         self.cur.execute("""CREATE TABLE auth_to_book(
         id SERIAL PRIMARY KEY,
-        book_id INT REFERENCES books(id),
-        auth_id INT REFERENCES authors(id)
+        book_id INT REFERENCES books(id) ON DELETE CASCADE,
+        auth_id INT REFERENCES authors(id) ON DELETE CASCADE
         );""")
         self.conn.commit()
+
+    # def add_book(self, lib_id, name, genre, author_names):
+    #     self.cur.execute("INSERT INTO books (lib_id, name, genre) VALUES (%s, %s, %s) RETURNING id;",(lib_id, name, genre))
+    #     book_id = self.cur.fetchone()[0]
+    #     for auth_name in author_names:
+    #         author = self.get_author(name=auth_name)
+    #         if author is None:
+    #             self.add_author(auth_name)
+    #             author = self.get_author(name=auth_name)
+    #         author_id = author[0]
+    #         self.cur.execute(
+    #             "INSERT INTO auth_to_book (book_id, auth_id) VALUES (%s, %s);",
+    #             (book_id, author_id)
+    #         )
+    #     self.conn.commit()
+    #     return book_id
 
     def add_book(self, lib_id, name, genre, author_names):
         self.cur.execute("INSERT INTO books (lib_id, name, genre) VALUES (%s, %s, %s) RETURNING id;",(lib_id, name, genre))
@@ -46,8 +62,8 @@ class DbInteraction:
         for auth_name in author_names:
             author = self.get_author(name=auth_name)
             if author is None:
-                self.add_author(auth_name)
-                author = self.get_author(name=auth_name)
+                self.conn.rollback()
+                return None
             author_id = author[0]
             self.cur.execute(
                 "INSERT INTO auth_to_book (book_id, auth_id) VALUES (%s, %s);",
@@ -60,6 +76,22 @@ class DbInteraction:
         self.cur.execute("SELECT * FROM books WHERE id =  %s;", (id,))
         book = self.cur.fetchone()
         return book
+
+    def update_book(self, id, name, genre):
+        self.cur.execute("SELECT * FROM books WHERE id = %s;", (id,))
+        book = self.cur.fetchone()
+        if not book:
+            return None
+        self.cur.execute("UPDATE books SET name = %s, genre = %s WHERE id = %s;", (name, genre, id))
+        self.conn.commit()
+        return self.cur.fetchone()
+
+    def delete_book(self, id):
+        self.cur.execute("DELETE FROM books WHERE id = %s", (id,))
+        if self.cur.rowcount == 0:
+            return False
+        self.conn.commit()
+        return True
 
     def add_author(self, name):
         self.cur.execute("INSERT INTO authors (name) VALUES (%s) RETURNING id;", (name,))
@@ -75,6 +107,22 @@ class DbInteraction:
         author = self.cur.fetchone()
         return author
 
+    def update_author(self, id, name):
+        self.cur.execute("SELECT * FROM authors WHERE id = %s;", (id,))
+        author = self.cur.fetchone()
+        if not author:
+            return None
+        self.cur.execute("UPDATE authors SET name = %s WHERE id = %s;", (name, id))
+        self.conn.commit()
+        return self.cur.fetchone()
+
+    def delete_author(self, id):
+        self.cur.execute("DELETE FROM authors WHERE id = %s", (id,))
+        if self.cur.rowcount == 0:
+            return False
+        self.conn.commit()
+        return True
+
     def add_library(self, name, address):
         self.cur.execute("INSERT INTO libraries (name, address) VALUES (%s, %s) RETURNING id;", (name, address))
         library_id = self.cur.fetchone()[0]
@@ -85,6 +133,22 @@ class DbInteraction:
         self.cur.execute("SELECT * FROM libraries WHERE id = %s;", (id,))
         library = self.cur.fetchone()
         return library
+
+    def update_library(self, id, name, address):
+        self.cur.execute("SELECT * FROM libraries WHERE id = %s;", (id,))
+        library = self.cur.fetchone()
+        if not library:
+            return None
+        self.cur.execute("UPDATE libraries SET name = %s, address = %s WHERE id = %s;", (name, address, id))
+        self.conn.commit()
+        return self.cur.fetchone()
+
+    def delete_library(self, id):
+        self.cur.execute("DELETE FROM libraries WHERE id = %s", (id,))
+        if self.cur.rowcount == 0:
+            return False
+        self.conn.commit()
+        return True
 
     def close(self):
         self.cur.close()
